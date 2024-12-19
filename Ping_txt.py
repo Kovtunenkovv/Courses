16d6cee81a5349077fcb393a760da3c8cdbeaa86
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2024-12-19 09:56:37 +0300

    Update Ping_txt.py
    add comment for linux/mac

--- Ping_txt.py
+++ Ping_txt.py
@@ -4,7 +4,7 @@ with open("IP.txt", 'r') as f_in:
     with open("IP_out.txt", 'w') as f_out: 
         for line in f_in:
             line = line.strip()
-            response = os.system(f"ping -n 1 {line}")
+            response = os.system(f"ping -n 1 {line}") #Windows '-n 1', Linux/macOS '-c 1'.
             if response == 0:
                 status = 'is up!'
             else:
